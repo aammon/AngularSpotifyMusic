1a72de271d8e165c9f180ed595b934d817d3a41d
aammon <aaron.ammon@sprint.com>
2017-03-09 13:59:08 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { AlbumComponent } from './components/album/album.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SearchComponent, NavbarComponent, AboutComponent, ArtistComponent, AlbumComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule, 
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
